825bb2e2a6d52361d0e9eb6c67d9844dcd545080
Alejandro Oviedo <alejandro.oviedo.g@gmail.com>
2020-10-19 12:19:22 -0300

    add grep alternative

--- src/file.ts
+++ src/file.ts
@@ -6,28 +6,29 @@ import { spawn } from 'child_process';
 import { Transform } from 'stream';
 import { Server, IncomingMessage, ServerResponse } from 'http';
 
-export default function (fastify: FastifyInstance, opts: FastifyPluginOptions, done: () => void): void {
-  fastify.get('/regex/:filename', {
-    schema: {
-      params: {
-        type: 'object',
-        required: ['filename'],
-        properties: {
-          filename: {
-            type: 'string',
-            minLength: 1
-          }
-        }
-      },
-      querystring: {
-        type: 'object',
-        properties: {
-          last: { type: 'number' },
-          keyword: { type: 'string' }
-        }
+const schema = {
+  params: {
+    type: 'object',
+    required: ['filename'],
+    properties: {
+      filename: {
+        type: 'string',
+        minLength: 1
       }
     }
-  }, getLogFile);
+  },
+  querystring: {
+    type: 'object',
+    properties: {
+      last: { type: 'number' },
+      keyword: { type: 'string' }
+    }
+  }
+};
+
+export default function (fastify: FastifyInstance, opts: FastifyPluginOptions, done: () => void): void {
+  fastify.get('/regex/:filename', { schema }, getLogFile);
+  fastify.get('/grep/:filename', { schema }, getLogFileUsingGrep);
   done();
 }
 
@@ -64,26 +65,22 @@ async function getLogFile(req: FastifyRequest<Request>, reply: FastifyReply) {
       path: resolvedPath
     }, 'Streaming file to response');
 
-    if(!keyword) {
-      reply.send(createReadStream(sortedFilePath));
+    if(keyword) {
+      reply.send(createReadStream(sortedFilePath).pipe(createFilterStream(keyword)));
     } else {
-      const transformer = new Transform({
-        transform(chunk, enc, callback) {
-          const filtered = chunk.toString()
-            .split('\n')
-            .filter((line: string) => line.match(new RegExp(keyword)))
-            .join('\n');
-          this.push(filtered);
-          callback();
-        }
-      });
-      reply.send(createReadStream(sortedFilePath).pipe(transformer));
+      reply.send(createReadStream(sortedFilePath));
     }
 
     (reply as any).raw.on('finish', () => {
       // TODO: schedule the file to be cleaned up or fire-and-forget the clean up process
     });
   } catch (error) {
+    if(error.code && error.code === 'ENOENT') {
+      reply.status(400);
+      return {
+        message: 'File not found'
+      };
+    }
     req.log.error({
       error
     }, 'Invalid request');
@@ -91,6 +88,61 @@ async function getLogFile(req: FastifyRequest<Request>, reply: FastifyReply) {
     reply.status(400);
     return {
       message: 'Invalid request'
+    };
+  }
+}
+
+async function getLogFileUsingGrep(req: FastifyRequest<Request>, reply: FastifyReply) {
+  const filename = req.params.filename;
+  const keyword = req.query.keyword;
+
+  try {
+    const resolvedPath = resolve('test', filename);
+
+    req.log.info({
+      path: resolvedPath
+    }, 'Resolved path');
+    // check if the file exists
+    const statRes = await fs.stat(resolvedPath);
+    req.log.info({
+      stat: statRes
+    }, 'Stat results');
+
+    let sortedFilePath: string;
+    if(keyword) {
+      sortedFilePath = await externalSortAndFilter(resolvedPath, keyword);
+    } else {
+      sortedFilePath = await externalSort(resolvedPath);
+    }
+
+    req.log.info({
+      path: resolvedPath
+    }, 'Streaming file to response');
+
+    reply.send(createReadStream(sortedFilePath));
+    (reply as any).raw.on('finish', () => {
+      // TODO: schedule the file to be cleaned up or fire-and-forget the clean up process
+      try {
+        fs.unlink(sortedFilePath);
+      } catch(error) {
+        req.log.error({
+          error
+        }, 'There was an error while trying to clean up temporal files');
+      }
+    });
+  } catch (error) {
+    if(error.code && error.code === 'ENOENT') {
+      reply.status(400);
+      return {
+        message: 'File not found'
+      };
+    }
+    req.log.error({
+      error
+    }, 'Internal error');
+    reply.status(500);
+    return {
+      message: 'Invalid server error'
     };
   }
 }
@@ -113,13 +165,22 @@ function filterInMemory(fileContent: string, keyword: string) {
   // TODO: implement
 }
 
-// function filterStream() {
+function externalSortAndFilter(filePath: string, keyword: string): Promise<string> {
+  const filename = uuidv4();
+  const resultingFilePath = `/tmp/${filename}.txt`;
 
-// }
+  const cmd = `sort -nr -t '${process.env.DELIMITER}' -k1 ${filePath} | grep ${keyword} > ${resultingFilePath}`;
+  const p = spawn('sh', ['-c', cmd]);
+  return new Promise((resolve, reject) => {
+    p.on('exit', code => {
+      if (code === 0) {
+        return resolve(resultingFilePath);
+      }
 
-// function grepFilter() {
-
-// }
+      return reject(new Error('Command failed'));
+    });
+  });
+}
 
 function externalSort(filePath: string): Promise<string> {
   const filename = uuidv4();
@@ -136,4 +197,18 @@ function externalSort(filePath: string): Promise<string> {
       return reject(new Error('Command failed'));
     });
   });
+}
+
+function createFilterStream(keyword: string): Transform {
+  const filter = new Transform({
+    transform(chunk, enc, callback) {
+      const filtered = chunk.toString()
+        .split('\n')
+        .filter((line: string) => line.match(new RegExp(keyword)))
+        .join('\n');
+      this.push(filtered);
+      callback();
+    }
+  });
+  return filter;
 }
